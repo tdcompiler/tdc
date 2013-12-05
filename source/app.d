bd3c78f9adcff7c373d002946f87639ab43040ad
Casper Faergemand <shorttail@hotmail.com>
2013-12-05 19:28:54 +0100

    Added basic grammar for passing initial tests and for running them.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,59 @@
-import std.stdio;
+import std.stdio, std.file, pegged.grammar;
 
-void main() { 
-	writeln("Edit source/app.d to start your project.");
+void main(string[] args) {
+	mixin(grammar(dGrammar));
+	foreach (string name; dirEntries("../tests", "*.d", SpanMode.depth)) {
+		auto s = readText(name);
+		auto r = D(s);
+		writeln(s);
+		writeln(r);
+		//writeln(r.successful);
+	}
 }
+
+enum dGrammar = `
+D:
+
+Module <- ModuleDecl? Stms?
+
+ModuleDecl < "module" qualifiedIdentifier ";"
+
+Stms < Stm+
+
+Stm < qualifiedIdentifier qualifiedIdentifier ";"
+	/ "{" Stms "}"
+	/ Function
+	/ ReturnStm
+
+Function < Type Name "(" ")" FunctionBody
+
+Type < "int"
+
+Name < "main"
+
+FunctionBody < "{" Stms "}"
+
+ReturnStm < "return" Exp? ";"
+
+Exp < Arithmetic
+
+Arithmetic < Factor (Addition / Subtraction)*
+Addition < "+" Factor
+Subtraction < "-" Factor
+Factor < Primary (Multiplication / Division)*
+Multiplication < "*" Primary
+Division < "/" Primary
+Primary < Parens / Negative / Positive / Number
+Parens < "(" Arithmetic ")"
+Negative < "-" Primary
+Positive < "+" Primary
+Number < Integer
+
+Integer < digit (digit / "_")*
+
+# Function < Identifier qualifiedIdentifier "(" ")" "{" Stms "}"
+
+#Identifier < Keyword 
+
+#Keyword < "int" / "return"
+`;
