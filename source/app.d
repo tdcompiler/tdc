588ebea6396543b1a9ee4cd25c0444a43ce537b7
Casper Faergemand <shorttail@hotmail.com>
2013-12-15 23:36:35 +0100

    Rewrote integers and floats according to specification

--- source/app.d
+++ source/app.d
@@ -54,11 +54,15 @@ void parse(string name, bool forceOutput = false) {
 		auto previousFile = readText(resultPath);
 		if (tree.toString == previousFile) {
 			if (forceOutput) {
+				writeln(name);
+				writeln(file);
 				writeln("Previous result identical to current result:");
 				writeln(treeText);
 			}
 			return;
 		}
+		writeln(name);
+		writeln(file);
 		writeln("Previous result:");
 		writeln(previousFile);
 		writeln("Current result:");
@@ -66,7 +70,6 @@ void parse(string name, bool forceOutput = false) {
 		write("Overwrite? (y/n): ");
 		char reply;
 		scanf("%s", &reply);
-		writeln(reply);
 		if (reply == 'y') {
 			writeln("Overwriting old file.");
 			resultPath.writeFile(treeText);
@@ -78,6 +81,8 @@ void parse(string name, bool forceOutput = false) {
 		 * No previous result existed. Program and parse tree are both
 		 * printed and the tree is stored.
 		 */
+		writeln(name);
+		writeln(file);
 		writeln("New test result:");
 		writeln(file);
 		writeln(tree);
@@ -94,57 +99,151 @@ ModuleDecl < "module" qualifiedIdentifier ";"
 
 Stms < Stm*
 
-Stm < qualifiedIdentifier qualifiedIdentifier ";"
-/ "{" Stms "}"
-/ Function
-/ ReturnStm
-/ Comment
+Stm < qualifiedIdentifier qualifiedIdentifier :";"
+	/ "{" Stms "}"
+	/ Function
+	/ ReturnStm
+	/ Comment
+	/ Assignment
 
-Function < Type Name "(" ")" FunctionBody
+Assignment < qualifiedIdentifier qualifiedIdentifier "=" Exp ";"
 
-Type < qualifiedIdentifier
+Function < Type Name :"(" :")" FunctionBody
+
+Type <- qualifiedIdentifier
 
 Name < qualifiedIdentifier
 
-FunctionBody < "{" Stms "}"
+FunctionBody < :"{" Stms :"}"
 
-ReturnStm < "return" Exp? ";"
+ReturnStm < "return" Exp? :";"
 
 Exp < Arithmetic
 
 Arithmetic < Factor (Addition / Subtraction)*
-Addition < "+" Factor
-Subtraction < "-" Factor
+Addition < :"+" Factor
+Subtraction < :"-" Factor
 Factor < Primary (Multiplication / Division / Modulo)*
-Multiplication < "*" Primary
-Division < "/" Primary
-Modulo < "%" Primary
+Multiplication < :"*" Primary
+Division < :"/" Primary
+Modulo < :"%" Primary
 Primary < Parens / Negative / Positive / Number
 Parens < "(" Arithmetic ")"
-Negative < "-" Primary
-Positive < "+" Primary
-Number < IntegerLiteral
+Negative < :"-" Primary
+Positive < :"+" Primary
+Number < IntegerLiteral / FloatLiteral
 
-Comment <: BlockComment # <: discards nodes. Replace with < to see nodes in tree
-/ LineComment
-/ NestingBlockComment
+# <: discards nodes. Replace with < to see nodes in tree
+Comment <: BlockComment
+	/ LineComment
+	/ NestingBlockComment
 
 BlockComment <~ :"/*" (!"*/" .)* :"*/"
 LineComment <~ :"//" (!eol .)* :eol
 NestingBlockComment <~ :"/+" (NestingBlockComment / (!("+/" / "/+") .))* :"+/"
 
-IntegerLiteral <- DecimalInteger
+FloatLiteral <- Float
+	/ Float Suffix
+	/ Integer ImaginarySuffix
+	/ Integer FloatSuffix ImaginarySuffix
+	/ Integer RealSuffix ImaginarySuffix
 
-DecimalInteger < Integer IntegerSuffix?
+Float <- DecimalFloat
+	/ HexFloat
+
+DecimalFloat <- LeadingDecimal "." DecimalDigits?
+	/ DecimalDigits "." DecimalDigitsNoSingleUS DecimalExponent
+	/ "." DecimalInteger DecimalExponent?
+	/ LeadingDecimal DecimalExponent
+
+DecimalExponent <- DecimalExponentStart DecimalDigitsNoSingleUS
+
+DecimalExponentStart <- "e" / "E" / "e+" / "E+" / "e-" / "E-"
+
+HexFloat <- HexPrefix HexDigitsNoSingleUS "." HexDigitsNoSingleUS HexExponent
+	/ HexPrefix "." HexDigitsNoSingleUS HexExponent
+	/ HexPrefix HexDigitsNoSingleUS HexExponent
+
+HexPrefix <- "0x" / "0X"
+
+HexExponent <- HexExponentStart DecimalDigitsNoSingleUS
+
+HexExponentStart <- "p" / "P" / "p+" / "P+" / "p-" / "P-"
+
+Suffix <- FloatSuffix
+	/ RealSuffix
+	/ ImaginarySuffix
+	/ FloatSuffix ImaginarySuffix
+	/ RealSuffix ImaginarySuffix
+
+FloatSuffix <- "f" / "F"
+
+RealSuffix <- "L"
+
+ImaginarySuffix <- "i"
+
+LeadingDecimal <- DecimalInteger
+	/ "0" DecimalDigitsNoSingleUS
+
+IntegerLiteral <- Integer IntegerSuffix?
+
+Integer <~ DecimalInteger
+	/ BinaryInteger
+	/ HexadecimalInteger
 
 IntegerSuffix <- "L" / "u" / "U" / "Lu" / "LU" / "uL" / "UL"
 
-Integer <- NonZeroDigit ( Digit / "_" )*
-/ Zero
+DecimalInteger <- "0"
+	/ NonZeroDigit DecimalDigitsUS?
 
-Digit < Zero / NonZeroDigit
+BinaryInteger <- BinPrefix BinaryDigits
 
-Zero <- "0"
+BinPrefix <- "0b" / "0B"
+
+HexadecimalInteger < HexPrefix HexDigitsNoSingleUS
 
 NonZeroDigit <- [1-9]
+
+DecimalDigits <- DecimalDigit+
+
+DecimalDigitsUS <~ DecimalDigitUS+
+
+DecimalDigitsNoSingleUS <- DecimalDigit DecimalDigitsUS?
+	/ DecimalDigitsUS DecimalDigit
+
+DecimalDigitNoStartingUS <- DecimalDigit DecimalDigitsUS?
+
+DecimalDigit <- "0" / NonZeroDigit
+
+DecimalDigitUS <- DecimalDigit / :"_"
+
+BinaryDigitsUS <- BinaryDigitUS+
+
+BinaryDigits <- BinaryDigit+
+
+BinaryDigit <- [0-1]
+
+BinaryDigitUS <- BinaryDigit / :"_"
+
+OctalDigits <- OctalDigit+
+
+OctalDigitsUS <- OctalDigit+
+
+OctalDigit <- [0-7]
+
+OctalDigitUS <- OctalDigit / :"_"
+
+HexDigits <- HexDigit+
+
+HexDigitsUS <- HexDigitUS+
+
+HexDigitUS <- HexDigit / :"_"
+
+HexDigitsNoSingleUS <- HexDigit HexDigitsUS?
+	/ HexDigitsUS HexDigit
+
+HexDigit <- DecimalDigit
+	/ HexLetter
+
+HexLetter <- [a-f] / [A-F] / :"_"
 `;
