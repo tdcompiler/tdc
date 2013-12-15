82298179928554c5a2494ecf04a97e0b293580d6
Casper Faergemand <shorttail@hotmail.com>
2013-12-15 20:35:46 +0100

    Improved testing framework

--- source/app.d
+++ source/app.d
@@ -1,13 +1,87 @@
-import std.stdio, std.file, pegged.grammar;
+import std.stdio,
+	   std.file,
+	   std.algorithm,
+	   std.regex,
+	   pegged.grammar;
+
+alias std.file.write writeFile;
+
+enum testDir = "../tests";
+enum resultDir = "../results";
 
 void main(string[] args) {
+	if (args.length == 1) {
+		foreach (string name; dirEntries(testDir, "*.d", SpanMode.depth)) {
+			parse(name);
+		}
+	} else {
+		foreach (string name; args[1 .. $]) {
+			parse(testDir ~ "/" ~ name, true);
+		}
+	}
+}
+
+void parse(string name, bool forceOutput = false) {
 	mixin(grammar(dGrammar));
-	foreach (string name; dirEntries("../tests", "*.d", SpanMode.depth)) {
-		auto file = readText(name);
-		auto tree = D(file);
-		//writeln(file);
-		//writeln(tree);
-		writeln(name," ",tree.successful);
+
+	/*
+	 * Replaces \ with / in path to give to unite Unix and Windows
+	 * syntax for string processing. Then the file itself is stripped
+	 * from the text and the middle path is preserved. If it does not
+	 * exist in the result folder, it will be generated.
+	 */
+	name = name.replaceAll!(a => "/")(regex(r"[\\]"));
+	auto resultPath = resultDir ~ name[testDir.length .. $];
+	auto middlePath = resultDir
+		~ name.dup[testDir.length .. $]
+		.reverse.find('/').reverse;
+	if (!middlePath.exists)
+		middlePath.mkdirRecurse;
+
+	/*
+	 * The target file is read and parsed.
+	 */
+	auto file = readText(name);
+	auto tree = D(file);
+	auto treeText = tree.toString;
+
+	if (resultPath.exists) {
+		/*
+		 * The previous result is read and compared to the new result.
+		 * If they are different, both are printed, and the user must
+		 * decide which result to keep.
+		 */
+		auto previousFile = readText(resultPath);
+		if (tree.toString == previousFile) {
+			if (forceOutput) {
+				writeln("Previous result identical to current result:");
+				writeln(treeText);
+			}
+			return;
+		}
+		writeln("Previous result:");
+		writeln(previousFile);
+		writeln("Current result:");
+		writeln(tree);
+		write("Overwrite? (y/n): ");
+		char reply;
+		scanf("%s", &reply);
+		writeln(reply);
+		if (reply == 'y') {
+			writeln("Overwriting old file.");
+			resultPath.writeFile(treeText);
+		} else {
+			writeln("Preserving old file.");
+		}
+	} else {
+		/*
+		 * No previous result existed. Program and parse tree are both
+		 * printed and the tree is stored.
+		 */
+		writeln("New test result:");
+		writeln(file);
+		writeln(tree);
+		resultPath.writeFile(treeText);
 	}
 }
 
@@ -21,10 +95,10 @@ ModuleDecl < "module" qualifiedIdentifier ";"
 Stms < Stm*
 
 Stm < qualifiedIdentifier qualifiedIdentifier ";"
-	/ "{" Stms "}"
-	/ Function
-	/ ReturnStm
-	/ Comment
+/ "{" Stms "}"
+/ Function
+/ ReturnStm
+/ Comment
 
 Function < Type Name "(" ")" FunctionBody
 
@@ -52,8 +126,8 @@ Positive < "+" Primary
 Number < IntegerLiteral
 
 Comment <: BlockComment # <: discards nodes. Replace with < to see nodes in tree
-	/ LineComment
-	/ NestingBlockComment
+/ LineComment
+/ NestingBlockComment
 
 BlockComment <~ :"/*" (!"*/" .)* :"*/"
 LineComment <~ :"//" (!eol .)* :eol
@@ -66,7 +140,7 @@ DecimalInteger < Integer IntegerSuffix?
 IntegerSuffix <- "L" / "u" / "U" / "Lu" / "LU" / "uL" / "UL"
 
 Integer <- NonZeroDigit ( Digit / "_" )*
-	/ Zero
+/ Zero
 
 Digit < Zero / NonZeroDigit
 
